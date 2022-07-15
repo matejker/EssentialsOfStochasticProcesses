a4a3f304f388df2a92cb3836deff87162f268544
Matej Kerekrety <matejkerekrety@datavant.com>
2022-07-15 11:21:19 +0100

    Chains with three or more states

---
+++ libs/stationary_distribution.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def get_stationary_distribution(p: np.ndarray) -> np.ndarray:
+    n, m = p.shape
+    if n != m:
+        raise ValueError(f"P is not a square matrix {n}!={m}")
+
+    # Two state markov chain
+    if n == 2:
+        return np.array([p[1][0], p[0][1]]) / (p[1][0] + p[0][1])
+
+    a = p - np.eye(n)
+    a[:, n - 1] = np.ones(n)
+
+    # It will raise LinAlgError if it is Singular matrix or other
+    return np.linalg.inv(a)[-1]
