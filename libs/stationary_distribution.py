ee3a3548c7a9c16ff9a161d3b70c207801167632
Matej Kerekrety <matejkerekrety@datavant.com>
2022-08-19 12:27:27 +0100

    Hitting times

--- libs/stationary_distribution.py
+++ libs/stationary_distribution.py
@@ -1,4 +1,5 @@
 import numpy as np
+from copy import deepcopy
 
 
 def get_stationary_distribution(p: np.ndarray, rates: bool = False) -> np.ndarray:
@@ -12,9 +13,9 @@ def get_stationary_distribution(p: np.ndarray, rates: bool = False) -> np.ndarra
     
     # Continues Time Markov chains with rates
     if rates:
-        a = p
+        a = deepcopy(p)
     else:
-        a = p - np.eye(n)
+        a = deepcopy(p - np.eye(n))
     
     a[:, n - 1] = np.ones(n)
 
@@ -23,10 +24,10 @@ def get_stationary_distribution(p: np.ndarray, rates: bool = False) -> np.ndarra
 
 
 def is_symmetric(a: np.ndarray) -> bool:
-    return a == a.T
+    return np.round(a, 4) == np.round(a.T, 4)
 
 
-def check_detailed_balance_condition(p: np.ndarray) -> bool:
-    pi = get_stationary_distribution(p)
+def check_detailed_balance_condition(p: np.ndarray, rates: bool = False) -> bool:
+    pi = get_stationary_distribution(p, rates=rates)
     return is_symmetric(pi * p.T)
 
